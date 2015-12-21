7faa9917c808d9cd3b38d5446eeb76c66a2a2f58
yaozeyuan <yaozeyuan93@gmail.com>
2015-12-21 23:19:13 +0800

    *    关闭了log模块 *    现在可以在windows下开发了：）

--- logconfig/logconfig.py
+++ logconfig/logconfig.py
@@ -3,12 +3,14 @@ https://github.com/jbalogh/zamboni/blob/master/log_settings.py
 """
 from __future__ import absolute_import
 
-from tornado.log import LogFormatter as TornadoLogFormatter
-import logging, logging.handlers
+import logging
+import logging.handlers
 import os.path
 import types
 
 from logconfig import dictconfig
+from tornado.log import LogFormatter as TornadoLogFormatter
+
 
 # Pulled from commonware.log we don't have to import that, which drags with
 # it Django dependencies.
@@ -17,37 +19,42 @@ class RemoteAddressFormatter(logging.Formatter):
 
     def format(self, record):
         if ('%(REMOTE_ADDR)' in self._fmt
-                and 'REMOTE_ADDR' not in record.__dict__):
+            and 'REMOTE_ADDR' not in record.__dict__):
             record.__dict__['REMOTE_ADDR'] = None
         return logging.Formatter.format(self, record)
+
 
 class UTF8SafeFormatter(RemoteAddressFormatter):
     def __init__(self, fmt=None, datefmt=None, encoding='utf-8'):
         logging.Formatter.__init__(self, fmt, datefmt)
         self.encoding = encoding
-    
+
     def formatException(self, e):
         r = logging.Formatter.formatException(self, e)
         if type(r) in [types.StringType]:
-            r = r.decode(self.encoding, 'replace') # Convert to unicode
+            r = r.decode(self.encoding, 'replace')  # Convert to unicode
         return r
-    
+
     def format(self, record):
         t = RemoteAddressFormatter.format(self, record)
         if type(t) in [types.UnicodeType]:
             t = t.encode(self.encoding, 'replace')
         return t
 
+
 class NullHandler(logging.Handler):
     def emit(self, record):
         pass
 
+
 def initialize_logging(syslog_tag, syslog_facility, loggers,
-        log_level=logging.INFO, use_syslog=False):
+                       log_level=logging.INFO, use_syslog=False):
     if os.path.exists('/dev/log'):
         syslog_device = '/dev/log'
     elif os.path.exists('/var/run/syslog'):
         syslog_device = '/var/run/syslog'
+    else:
+        syslog_device = './'
 
     base_fmt = ('%(name)s:%(levelname)s %(message)s:%(pathname)s:%(lineno)s')
 
