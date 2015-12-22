e87ac01b487d5539c7e9c8e0d0e4e0fb4711c959
姚泽源 <yaozeyuan93@gmail.com>
2015-12-22 21:48:25 +0800

    *    web版的坑比我想的深

--- handlers/login.py
+++ handlers/login.py
@@ -7,3 +7,9 @@ logger = logging.getLogger('boilerplate.' + __name__)
 class LoginHandler(BaseHandler):
     def get(self):
         self.render("login/login.html")
+
+    def post(self):
+        account = self.get_argument('account')
+        password = self.get_argument('password')
+        captcha = self.get_argument('captcha',default=None)
+        return
